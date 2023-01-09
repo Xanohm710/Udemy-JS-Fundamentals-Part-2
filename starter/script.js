7301877498d0a6b122574a5752b35e9378ef058d
Jared Young <104388797+Xanohm710@users.noreply.github.com>
2023-01-09 10:10:52 -0600

    Activating Strict Mode

---
+++ starter/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
+// Strict mode throws errors for certain words that could be used in the future
+// const interface = 'Audio';
+// const private = 534;
+// const if = 23;
+
