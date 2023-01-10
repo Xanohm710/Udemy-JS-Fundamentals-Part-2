d7a70d4588bc99b4145d9d817e92d57e503efccb
Jared Young <104388797+Xanohm710@users.noreply.github.com>
2023-01-10 07:45:01 -0500

    Reviewing Functions

--- starter/script.js
+++ starter/script.js
@@ -64,7 +64,6 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 
 console.log(yearsUntilRetirement(1993, 'Erin'));
 console.log(yearsUntilRetirement(1992, 'Jared'));
-*/
 
 function cutFruitPieces(fruit) {
     return fruit * 4;
@@ -78,3 +77,25 @@ function fruitProcessor(apples, oranges) {
     return juice;
 }
 console.log(fruitProcessor(2, 3));
+*/
+
+const calcAge = function (birthYear) {
+    return 2023 - birthYear;
+}
+
+const yearsUntilRetirement = function (birthYear,
+    firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 50 - age;
+
+    if(retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    } else {
+        return -1;
+        console.log(`${firstName} has already retired!!`);
+    }
+}
+
+console.log(yearsUntilRetirement(1993, 'Erin'));
+console.log(yearsUntilRetirement(1992, 'Jared'));
