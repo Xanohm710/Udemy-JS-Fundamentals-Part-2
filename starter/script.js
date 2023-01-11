747a2b161783aa5047d8e3fed5a9c27aa0ae8cfb
Jared Young <104388797+Xanohm710@users.noreply.github.com>
2023-01-11 12:30:53 -0500

    The while Loop

--- starter/script.js
+++ starter/script.js
@@ -462,7 +462,6 @@ for(let i = 0; i < jared.length; i++) {
 
     console.log(jared[i], typeof jared[i]);
 }
-*/
 
 // Looping Backwards and Loops in Loops
 
@@ -487,4 +486,26 @@ for (let exercise = 1; exercise < 4; exercise++) {
     for (let rep = 1; rep < 6; rep++) {
         console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`);
     }
+}
+*/
+
+// The while Loop
+
+// for(let rep = 1; rep <= 10; rep++) {
+//     console.log(`Lifting weights repetition ${rep}`);
+// }
+
+let rep = 1;
+while (rep <= 10) {
+    //console.log(`WHILE: Lifting weights repetition ${rep}`);
+    rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6) console.log('Loop is ending...');
 }
